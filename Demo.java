382e52d5aec13ffa7a7c50a1c9408f26275f300f
HFH-1 <58933770+HFH-1@users.noreply.github.com>
2019-12-17 21:24:09 +0800

    创建Demo.java文件

---
+++ Demo.java
@@ -0,0 +1,11 @@
+package com.leyou.helloSpringBoot.Controller;
+
+/**
+ * @auther HuangFeiHong
+ * @date 2019-12-17 21:20
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
